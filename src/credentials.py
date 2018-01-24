3fe11c62a13b07e4782123e4f9089a6b2e037025
Aroogz <arogeakindele@gmail.com>
2018-01-24 17:01:01 +0200

    config. credentials

---
+++ src/credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = '1m6d5DLHkGO9gwmPnENrAUcTt'
+CONSUMER_SECRET = '13523Tr36vpVQqGcjAO0eaEcnXbd3EU99wNv5mKjcDITiJm8Nz'
+
+# Access:
+ACCESS_TOKEN  = '141239888-SWgYb06yxEbkyaBae9aIJ5Ri0ueFrm0U00xolyhe'
+ACCESS_SECRET = 'tFuRQo781MD9VPV15ieH0QbvRjOTgE2McSTa7FWrgLXiC'
